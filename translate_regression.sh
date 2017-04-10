a7bbb4a2b6cee877e732c2498b4b79e209d6a143
fyabc <fyabc@mail.ustc.edu.cn>
2017-04-10 20:34:09 +0800

    Fix in regression shell.

--- translate_regression.sh
+++ translate_regression.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 mkdir -p translated/init
-python translate.py model/$1 data/dic/filtered_dic_en-fr.en.pkl data/dic/filtered_dic_en-fr.fr.pkl data/test/test_en-fr.en.tok translated/$2 -p $3 -k 4
+mkdir -p translated/regression_init
+python translate.py $1 data/dic/filtered_dic_en-fr.en.pkl data/dic/filtered_dic_en-fr.fr.pkl data/test/test_en-fr.en.tok translated/$2 -p $3 -k 4
 perl multi-bleu.perl data/test/test_en-fr.fr.tok < translated/$2
 
