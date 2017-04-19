5e68daae1ea73fee59de9f134b70d4f321e5f178
fyabc <fyabc@mail.ustc.edu.cn>
2017-04-19 21:44:59 +0800

    Another fix.

--- translate_regression.sh
+++ translate_regression.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
-# Usage: translate_regression.sh gpu_id n_enc n_dec residual_enc attention iteration
+# Usage: translate_regression.sh gpu_id n_enc n_dec residual_enc attention iteration n_processes
 
 export THEANO_FLAGS=device=gpu$1
-mkdir -p translated/init
-mkdir -p translated/regression_init
-python translate.py /datadrive/v-yanfa/model/complete/e${2}d${3}_res${4}_att${5}.iter${6}.npz data/dic/filtered_dic_en-fr.en.pkl data/dic/filtered_dic_en-fr.fr.pkl data/test/test_en-fr.en.tok translated/complete/e${2}d${3}_res${4}_att${5}_iter${6}.tok -k 4
+mkdir -p translated/complete
+python translate.py /datadrive/v-yanfa/model/complete/e${2}d${3}_res${4}_att${5}.iter${6}.npz data/dic/filtered_dic_en-fr.en.pkl data/dic/filtered_dic_en-fr.fr.pkl data/test/test_en-fr.en.tok translated/complete/e${2}d${3}_res${4}_att${5}_iter${6}.tok -k 4 -p ${7}
 perl multi-bleu.perl data/test/test_en-fr.fr.tok < translated/complete/e${2}d${3}_res${4}_att${5}_iter${6}.tok
 
--- translate_single.sh
+++ translate_single.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
+# Usage: translate_single.sh gpu_id n_enc n_dec residual_enc attention iteration
+
 mkdir -p translated/complete
 
-python translate_single.py $1 data/dic/filtered_dic_en-fr.en.pkl data/dic/filtered_dic_en-fr.fr.pkl data/test/test_en-fr.en.tok translated/$2 -p $3 -k 4
-perl multi-bleu.perl data/test/test_en-fr.fr.tok < translated/$2
+python translate_single.py /datadrive/v-yanfa/model/complete/e${2}d${3}_res${4}_att${5}.iter${6}.npz data/dic/filtered_dic_en-fr.en.pkl data/dic/filtered_dic_en-fr.fr.pkl data/test/test_en-fr.en.tok translated/complete/e${2}d${3}_res${4}_att${5}_iter${6}.tok -k 4
+perl multi-bleu.perl data/test/test_en-fr.fr.tok < translated/complete/e${2}d${3}_res${4}_att${5}_iter${6}.tok
