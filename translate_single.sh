564c0e9cf7bfd5588ab6e18adb9a17d0931b231c
Ubuntu <drl@drl0b.5egmxwdtkmtuhnk3vietjbrfif.jx.internal.cloudapp.net>
2017-04-23 11:02:19 +0000

    Another fix

--- translate_single.sh
+++ translate_single.sh
@@ -4,5 +4,5 @@
 
 export THEANO_FLAGS=device=gpu$1
 mkdir -p translated/complete
-python translate_single.py /datadrive/v-yanfa/model/complete/e${2}d${3}_res${4}_att${5}${7}.iter${6}.npz data/dic/filtered_dic_en-fr.en.pkl data/dic/filtered_dic_en-fr.fr.pkl data/test/test_en-fr.en.tok translated/complete/e${2}d${3}_res${4}_att${5}{$7}_iter${6}.tok -k 4
+python translate_single.py /datadrive/v-yanfa/model/complete/e${2}d${3}_res${4}_att${5}${7}.iter${6}.npz data/dic/filtered_dic_en-fr.en.pkl data/dic/filtered_dic_en-fr.fr.pkl data/test/test_en-fr.en.tok translated/complete/e${2}d${3}_res${4}_att${5}${7}_iter${6}.tok -k 4
 perl multi-bleu.perl data/test/test_en-fr.fr.tok < translated/complete/e${2}d${3}_res${4}_att${5}${7}_iter${6}.tok
