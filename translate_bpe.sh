44c8555e6c0f537a6d1ed74a96b2cc1257a950d0
fyabc <fyabc@mail.ustc.edu.cn>
2017-04-27 13:54:53 +0800

    Fix in bpe translate scripts.

--- translate_bpe.sh
+++ translate_bpe.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# Usage: translate_single_bpe.sh gpu_id n_enc n_dec residual_enc attention iteration thread_num [other]
+# Usage: translate_bpe.sh gpu_id n_enc n_dec residual_enc attention iteration thread_num [other]
 
 export THEANO_FLAGS=device=gpu$1,floatX=float32
 mkdir -p translated/complete
@@ -10,6 +10,6 @@ output_file=translated/complete/${model_name}_iter${6}.tok
 
 python translate.py //gcr/Scratch/RR1/v-yanfa/SelectiveTrain/model/complete/${model_name}.iter${6}.npz data/dic/filtered_dic_en-fr.en.pkl data/dic/filtered_dic_en-fr.fr.pkl data/test/test_en-fr.en.tok ${output_file} -k 4 -p ${7}
 
-cat ${output_file} | sed -r 's/(@@ )|(@@ ?$)//g' > ${output_file}
+cat ${output_file} | sed -r 's/(@@ )|(@@ ?$)//g' > ${output_file}.bpe
 
-perl multi-bleu.perl data/test/test_en-fr.fr.tok < ${output_file}
+perl multi-bleu.perl data/test/test_en-fr.fr.tok < ${output_file}.bpe
--- translate_single_bpe.sh
+++ translate_single_bpe.sh
@@ -10,6 +10,6 @@ output_file=translated/complete/${model_name}_iter${6}.tok
 
 python translate_single.py /datadrive/v-yanfa/model/complete/${model_name}.iter${6}.npz data/dic/filtered_dic_en-fr.en.pkl data/dic/filtered_dic_en-fr.fr.pkl data/test/test_en-fr.en.tok ${output_file} -k 4
 
-cat ${output_file} | sed -r 's/(@@ )|(@@ ?$)//g' > ${output_file}
+cat ${output_file} | sed -r 's/(@@ )|(@@ ?$)//g' > ${output_file}.bpe
 
-perl multi-bleu.perl data/test/test_en-fr.fr.tok < ${output_file}
+perl multi-bleu.perl data/test/test_en-fr.fr.tok < ${output_file}.bpe
