edf838057d8e1d78e0c77c99a9d47fb9465d36d6
fyabc <fyabc@mail.ustc.edu.cn>
2017-05-08 13:52:19 +0800

    Typo fix.

--- train_en_de.bat
+++ train_en_de.bat
@@ -1,5 +1,5 @@
 @rem Train BPE of en-de.
-python train_nmt.py %* --train1 en-de.en_0 --train2 en-de_de.0 ^
+python train_nmt.py %* --train1 en-de.en_0 --train2 en-de.de_0 ^
 --valid1 dev_en.tok --valid2 dev_de.tok ^
 --dic1 en-de.en.pkl --dic2 en-de.de.pkl ^
---small1 small_en-de.en_0 --small2 small_en-de_de.0
+--small1 small_en-de.en_0 --small2 small_en-de.de_0
