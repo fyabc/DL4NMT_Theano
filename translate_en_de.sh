35eadcd96e4a29bc248daf4a34c63dbe4de9a590
fyabc <fyabc@mail.ustc.edu.cn>
2017-05-10 13:20:38 +0800

    Fix the bug of translate_en_de.sh.

--- translate_en_de.sh
+++ translate_en_de.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-# Usage: translate_en_de.bat n_enc n_dec residual_enc attention iteration n_processes [postfix]
-python translate.py /datadrive/v-yanfa/model/complete/e${1}d${2}_res${3}_att${4}${7}.iter${5}.npz data/dic/en-de.en.pkl data/dic/en-de.de.pkl data/test/newstest2014-deen-src.en.tok translated/complete/e${1}d${2}_res${3}_att${4}${7}_iter${5}.tok -p ${6} -k 4
+# Usage: translate_en_de.bat gpu_id n_enc n_dec residual_enc attention iteration n_processes [postfix]
+export THEANO_FLAGS=device=gpu$1,floatX=float32
+mkdir -p translated/complete
+python translate.py /datadrive/v-yanfa/model/complete/e${2}d${3}_res${4}_att${5}${8}.iter${6}.npz data/dic/en-de.en.pkl data/dic/en-de.de.pkl data/test/newstest2014-deen-src.en.tok translated/complete/e${2}d${3}_res${4}_att${5}${8}_iter${6}.tok -p ${7} -k 4
 
 # calculate multi BLEU score
-perl multi-bleu.perl data/test/newstest2014-deen-ref.de.tok < translated/complete/e${1}d${2}_res${3}_att${4}${7}_iter${5}.tok
+perl multi-bleu.perl data/test/newstest2014-deen-ref.de.tok < translated/complete/e${2}d${3}_res${4}_att${5}${8}_iter${6}.tok
