26a938dd5e844419a618d24cdd0dbca1b7ebacc0
Ubuntu <drl@drl0d.5egmxwdtkmtuhnk3vietjbrfif.jx.internal.cloudapp.net>
2017-05-24 14:07:09 +0000

    Bug fix

--- translate_bpe_en_fr_single.sh
+++ translate_bpe_en_fr_single.sh
@@ -8,7 +8,7 @@ mkdir -p translated/complete
 model_name=e${2}d${3}_res${4}_att${5}${7}
 output_file=translated/complete/${model_name}_iter${6}.tok
 
-python translate.py /datadrive/v-yanfa/model/complete/${model_name}.iter${6}.npz data/dic/en-fr_vocab.bpe.32000.pkl data/dic/en-fr_vocab.bpe.32000.pkl data/test/test_en-fr.en.tok.bpe.32000 ${output_file} -k 4
+python translate_single.py /datadrive/v-yanfa/model/complete/${model_name}.iter${6}.npz data/dic/en-fr_vocab.bpe.32000.pkl data/dic/en-fr_vocab.bpe.32000.pkl data/test/test_en-fr.en.tok.bpe.32000 ${output_file} -k 4
 
 cat ${output_file} | sed -r 's/(@@ )|(@@ ?$)//g' > ${output_file}.bpe
 
