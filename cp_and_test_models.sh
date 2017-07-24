c18575647f069fc3890548544581f4110bba8d4c
ustctf <feitian@outlook.com>
2017-07-24 22:03:58 +0800

    Merge Jianxin's modification for the bash testing script.

--- cp_and_test_models.sh
+++ cp_and_test_models.sh
@@ -5,7 +5,7 @@ end=$4
 task=$5
 for fid in $(seq ${start} 10000 ${end})
 do
-    pscp -pw fetia fetia@${ip}:/data/users/fetia/DL4NMT_Theano/model/complete/${modelname}.iter${fid}.* model/complete
+    scp fetia@${ip}:~/fetia/DL4NMT_Theano/model/complete/${modelname}.iter${fid}.* model/complete
 done
 startdivide=$(expr ${start} / 10000)
 enddivide=$(expr ${end} / 10000)
