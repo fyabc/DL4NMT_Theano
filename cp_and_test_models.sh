12ced8de8ad289b1bd8e050bdca6fd62afe6ffcb
ustctf <feitian@outlook.com>
2017-07-24 09:41:32 +0800

    Add the testing bash script.

---
+++ cp_and_test_models.sh
@@ -0,0 +1,12 @@
+ip=$1
+modelname=$2
+start=$3
+end=$4
+task=$5
+for fid in $(seq ${start} 10000 ${end})
+do
+    pscp -pw fetia fetia@${ip}:/data/users/fetia/DL4NMT_Theano/model/complete/${modelname}.iter${fid}.* model/complete
+done
+startdivide=$(expr ${start} / 10000)
+enddivide=$(expr ${end} / 10000)
+python seq_translate.py model/complete/${modelname}.npz --start ${startdivide} --end ${enddivide} --dataset ${task} --beam 6
