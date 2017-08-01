81fd0c057e8b64cface369e24162dd54d62fa541
ustctf <feitian@outlook.com>
2017-08-01 18:49:17 +0800

    Add cost_normalization in translating.

--- libs/models/model.py
+++ libs/models/model.py
@@ -305,7 +305,9 @@ class NMTModel(object):
         self.O = options
         if 'fix_dp_bug' not in options:
             self.O['fix_dp_bug'] = False
-
+        if 'cost_normalization' not in options:
+            self.O['cost_normalization'] = 1
+            
         # Dict of parameters (Theano shared variables)
         self.P = OrderedDict() if given_params is None else given_params
 
