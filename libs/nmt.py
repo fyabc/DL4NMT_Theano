8b78e1219a3f07e89a2d68728dcb95f9d11beacd
Renqian Luo <lrqrichard@gmail.com>
2017-10-13 11:26:22 +0800

    modify finetune

--- libs/nmt.py
+++ libs/nmt.py
@@ -540,13 +540,13 @@ Start Time = {}
                             if finetune_cnt % 2 == 0:
                                 lrate = np.float32(lrate * 0.1)
                                 message('Discount learning rate to {} at iteration {} at workder {}'.format(lrate, uidx, worker_id))
-                                if lrate <= 0.005:
-                                    message('Learning rate decayed to {:.5f}, task completed'.format(lrate))
-                                    return 1., 1., 1.
                             else:
                                 clip_shared.set_value(np.float32(clip_shared.get_value() * 0.1))
                                 message('Discount clip value to {} at iteration {}'.format(clip_shared.get_value(), uidx))
-                            finetune_cnt += 1
+                            finetune_cnt += 1  
+                            if finetune_cnt >= 3:
+                                message('Learning rate decayed to {:.5f}, clip decayed to {:.5f}, task completed'.format(lrate, clip_shared.get_value()))
+                                return 1., 1., 1.
                             bad_counter = 0
 
             # finish after this many updates
