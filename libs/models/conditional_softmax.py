Bug fix of applying delib_vocab on LSTM model and conditional softmax model.

libs/models/conditional_softmax.py
@@ -14,7 +14,7 @@ from ..layers.basic import _slice
 from ..constants import fX, profile
 from ..utility.basic import floatX
 from ..utility.utils import *
-from ..utility.theano_arg_top_k import theano_argpartsort
+from ..utility.theano_ops import theano_argpartsort, theano_unique
 
 __author__ = 'fyabc'
 
@@ -277,25 +277,31 @@ class ConditionalSoftmaxModel(DelibNMT):
             [y_pos_.shape[0], y_pos_.shape[1], self.O['dim_word']])
 
         # Per-word prediction decoder.
-        with delib_env(self):
-            y_mask = T.alloc(floatX(1.), self.O['maxlen'], x.shape[0])
+        y_mask = T.alloc(floatX(1.), self.O['maxlen'], x.shape[0])
 
-            # Output: probabilities of target words in this batch, ([Tt], [Bs], [V_tgt])
-            pw_probs = self.independent_decoder(tgt_pos_embed, y_mask, ctx, x_mask,
-                                                trng=trng, use_noise=use_noise, softmax=False)
-            k = self.O['cond_softmax_k']
-            top_k_args = theano_argpartsort(-pw_probs, k, axis=1)[:, :k]
-            # [NOTE] indices to get/set top-k value
-            top_k_indices = (T.arange(pw_probs.shape[0]).dimshuffle([0, 'x']), top_k_args)
+        # Output: probabilities of target words in this batch, ([Tt], [Bs], [V_tgt])
+        pw_probs = self.independent_decoder(tgt_pos_embed, y_mask, ctx, x_mask,
+                                            trng=trng, use_noise=use_noise, softmax=False)
+        k = self.O['cond_softmax_k']
+        top_k_args = theano_argpartsort(-pw_probs, k, axis=1)[:, :k]
 
-            # TODO: union top-k indices in each sentences
+        # Top-k vocabulary of this batch. ([Total vocab size of a batch],)
+        top_k_vocab_tensor = theano_unique(top_k_args)
+        # Shared variable for f_next. ([V_tgt],), 0-1 indicator
+        top_k_vocab_indicator = theano.shared(np.empty([self.O['n_words']], dtype='int64'), name='top_k_vocab')
 
         print 'Building f_init...',
         inps = [x]
         if batch_mode:
             inps.append(x_mask)
         outs = [init_state, ctx]
-        f_init = theano.function(inps, outs, name='f_init', profile=profile,)
+        f_init = theano.function(
+            inps, outs,
+            name='f_init', profile=profile,
+            updates={
+                top_k_vocab_indicator: T.set_subtensor(T.zeros_like(top_k_vocab_indicator)[top_k_vocab_tensor], 1)
+            },
+        )
         print 'Done'
 
         pre_projected_context_ = self.attention_projected_context(ctx, prefix='decoder')
@@ -338,14 +344,18 @@ class ConditionalSoftmaxModel(DelibNMT):
         if dropout_rate_out:
             logit = self.dropout(logit, use_noise, trng, dropout_rate_out)
 
-        logit = self.feed_forward(logit, prefix='ff_logit', activation=linear)
+        logit = self.feed_forward(logit, prefix='ff_logit', activation=linear)  # ([Bs], [V_tgt])
+
+        row_index = T.arange(y.shape[0]).dimshuffle([0, 'x'])
+        # Restore vocab from indicator.
+        top_k_vocab = T.nonzero(top_k_vocab_indicator)[0]
+        part_logit = logit[row_index, top_k_vocab]
 
         # Compute the softmax probability
-        # TODO: apply per-word output on it
-        next_probs = T.nnet.softmax(logit)
+        next_probs = T.nnet.softmax(part_logit)
 
         # Sample from softmax distribution to get the sample
-        next_sample = trng.multinomial(pvals=next_probs).argmax(1)
+        next_sample = top_k_vocab[trng.multinomial(pvals=next_probs).argmax(1)]
 
         # Compile a function to do the whole thing above, next word probability,
         # sampled word for the next target, next hidden state to be used

libs/models/model.py
@@ -23,6 +23,7 @@ from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 from ..constants import fX, profile
 from ..config import DefaultOptions
 from ..utility.utils import *
+from ..utility.theano_ops import theano_unique
 from ..layers import *
 
 __author__ = 'fyabc'
@@ -573,6 +574,7 @@ class NMTModel(object):
             # delib_vocab: ([#sentences], [k]); current_index: shared variable of current index of the delib_vocab
             delib_vocab = T.constant(np.load(delib_vocab_file)['delib_vocab'], name='delib_vocab')
             current_index = theano.shared(0, name='current_index')
+            batch_size = theano.shared(0, name='batch_size')
 
         # Word embedding for forward rnn and backward rnn (source)
         src_embedding = self.embedding(x, n_timestep, n_samples)
@@ -597,7 +599,10 @@ class NMTModel(object):
         f_init = theano.function(
             inps, outs,
             name='f_init', profile=profile,
-            updates={current_index: current_index + n_samples} if delib_vocab_file else None,
+            updates={
+                current_index: current_index + n_samples,
+                batch_size: n_samples,
+            } if delib_vocab_file else None,
         )
         print('Done')
 
@@ -643,7 +648,7 @@ class NMTModel(object):
         if delib_vocab_file:
             row_index = T.arange(y.shape[0]).dimshuffle([0, 'x'])
 
-            part_delib_vocab = delib_vocab[current_index: current_index + y.shape[0]]
+            part_delib_vocab = theano_unique(delib_vocab[current_index: current_index + batch_size])
             part_logit = logit[row_index, part_delib_vocab]
 
             # Compute the softmax probability
@@ -652,7 +657,7 @@ class NMTModel(object):
             # Sample from softmax distribution to get the sample
             next_sample_top_k = trng.multinomial(pvals=next_probs).argmax(1)
 
-            next_sample = part_delib_vocab[row_index, next_sample_top_k.dimshuffle([0, 'x'])].flatten()
+            next_sample = part_delib_vocab[next_sample_top_k]
         else:
             # Compute the softmax probability
             next_probs = T.nnet.softmax(logit)

libs/utility/theano_ops.py
@@ -1,8 +1,9 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-"""The Theano operator of arg-top-k."""
+"""Some Theano operations."""
 
+import numpy as np
 import theano
 import theano.tensor as T
 
@@ -80,7 +81,14 @@ def theano_argpartsort(a, k, axis=-1):
     return ArgPartSortOp(k)(a, axis)
 
 
+@theano.as_op(itypes=[T.imatrix], otypes=[T.ivector])
+def theano_unique(a):
+    """Apply symbolic unique operation"""
+    return np.unique(a)
+
+
 __all__ = [
     'arg_top_k',
     'theano_argpartsort',
+    'theano_unique',
 ]